6db1f5b7257d14880eb163a8db4567b76352f6ee
Your Name <you@example.com>
2021-11-17 16:11:34 +0545

    python introduction

---
+++ introduction.py
@@ -0,0 +1,17 @@
+# print("Hello python class")
+name="Anupam"
+lastname="chauhan"
+age=17
+
+
+
+
+#old way
+print("\"My name is {} {} age{}\"".format(name,lastname,age))
+
+print(f"\"Hello my name is :{name}  {lastname} and my age is {age}\"")
+
+# note 
+# 1)variable will alawys be alphanumeric
+# 2)
+
---
+++ yourname.py
@@ -0,0 +1,6 @@
+name=input("Enter your name :")
+age=input("Enter your age :")
+address=input("Enter your address :")
+phone_num=input("Enter you phone number :")
+
+print(f"Your name is {name}.Your age is {age}.You live in {address}.And finally your phone number is {phone_num}")
